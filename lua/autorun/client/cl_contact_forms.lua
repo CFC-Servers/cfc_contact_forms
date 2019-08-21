42fb751c31af408c4056ae48cfcce17e30e5086a
Brandon Sturgeon <sturgeonb4@gmail.com>
2019-08-20 22:25:46 -0400

    Add console command to toggle

--- lua/autorun/client/cl_contact_forms.lua
+++ lua/autorun/client/cl_contact_forms.lua
@@ -1,142 +1,146 @@
-local Frame = vgui.Create( "DFrame" )
-Frame:SetTitle( "Contact Forms" )
-Frame:SetSize( 300, 300 )
-Frame:Center()
-Frame:MakePopup()
-
 local LEFT_BORDER = 10
 
 local function makeFormButton(text, pos, callback, parent)
-	local Button = vgui.Create( "DButton", parent )
+    local Button = vgui.Create( "DButton", parent )
 
-	Button:SetText( text )
-	Button:SetTextColor( Color( 255, 255, 255 ) )
-	Button:SetPos( LEFT_BORDER + pos.x, pos.y )
+    Button:SetText( text )
+    Button:SetTextColor( Color( 255, 255, 255 ) )
+    Button:SetPos( LEFT_BORDER + pos.x, pos.y )
 
-	local parentX = parent:GetSize()
-	local desiredSizeX = parentX - ( LEFT_BORDER * 2 )
+    local parentX = parent:GetSize()
+    local desiredSizeX = parentX - ( LEFT_BORDER * 2 )
 
-	Button:SetSize( desiredSizeX, 30 )
+    Button:SetSize( desiredSizeX, 30 )
 
-	Button.Paint = function( self, w, h )
-		draw.RoundedBox( 0, 0, 0, w, h, Color( 41, 128, 185, 250 ) )
-	end
+    Button.Paint = function( self, w, h )
+        draw.RoundedBox( 0, 0, 0, w, h, Color( 41, 128, 185, 250 ) )
+    end
 
-	Button.DoClick = callback
+    Button.DoClick = callback
 
-	return Button
+    return Button
 end
 
 FORM_TYPE_TO_NETSTRING = {
-	[ "contact" ] = "CFC_SubmitContactForm",
-	[ "feedback" ] = "CFC_SubmitFeedbackForm",
-	[ "bug-report" ] = "CFC_SubmitBugReport",
-	[ "player-report" ] = "CFC_SubmitPlayerReport"
+    [ "contact" ] = "CFC_SubmitContactForm",
+    [ "feedback" ] = "CFC_SubmitFeedbackForm",
+    [ "bug-report" ] = "CFC_SubmitBugReport",
+    [ "player-report" ] = "CFC_SubmitPlayerReport"
 }
 
 local function processFieldsForForm( fields, formData )
-	local formType = formData.formType
-	local netstring = FORM_TYPE_TO_NETSTRING[formType]
+    local formType = formData.formType
+    local netstring = FORM_TYPE_TO_NETSTRING[formType]
 
-	net.Start( netstring )
-		for _, field in pairs( fields ) do
-			net.WriteString( field:GetValue() )
-		end
-	net.SendToServer()
+    net.Start( netstring )
+        for _, field in pairs( fields ) do
+            net.WriteString( field:GetValue() )
+        end
+    net.SendToServer()
 
 end
 
 local function openForm( formData )
-	Frame:Close()
+    Frame:Close()
 
-	local Form = vgui.Create( "DFrame" )
-	Form:SetTitle( formData.title )
-	Form:SetSize( 600, 600 )
-	Form:Center()
-	Form:MakePopup()
+    local Form = vgui.Create( "DFrame" )
+    Form:SetTitle( formData.title )
+    Form:SetSize( 600, 600 )
+    Form:Center()
+    Form:MakePopup()
 
-	local baseHeight = 30
+    local baseHeight = 30
 
-	local fields = {}
+    local fields = {}
 
-	local ycounter = baseHeight
+    local ycounter = baseHeight
 
-	for i, question in pairs( formData.questions ) do
-		local query = question.query
-		local fieldType = question.fieldType
+    for i, question in pairs( formData.questions ) do
+        local query = question.query
+        local fieldType = question.fieldType
 
-		local label = vgui.Create( "DLabel", Form )
-		label:SetPos( LEFT_BORDER, ycounter )
-		label:SetSize( Form:GetSize() * 0.95, 15 )
-		label:SetText( query )
+        local label = vgui.Create( "DLabel", Form )
+        label:SetPos( LEFT_BORDER, ycounter )
+        label:SetSize( Form:GetSize() * 0.95, 15 )
+        label:SetText( query )
 
-		local field = vgui.Create( "DTextEntry", Form )
-		field:SetPos( LEFT_BORDER, ycounter + 15 )
-		field:SetSize( Form:GetSize() * 0.95, 35 )
+        local field = vgui.Create( "DTextEntry", Form )
+        field:SetPos( LEFT_BORDER, ycounter + 15 )
+        field:SetSize( Form:GetSize() * 0.95, 35 )
 
-		ycounter = ycounter + 60
+        ycounter = ycounter + 60
 
-		fields[question.name] = field
-	end
+        fields[question.name] = field
+    end
 
-	local Submit = vgui.Create( "DButton", Form )
-	Submit:SetText( "Submit" )
-	Submit:SetPos( LEFT_BORDER, ycounter )
+    local Submit = vgui.Create( "DButton", Form )
+    Submit:SetText( "Submit" )
+    Submit:SetPos( LEFT_BORDER, ycounter )
 
-	Submit.DoClick = function()
-		local result = processFieldsForForm( fields, formData )
-	end
+    Submit.DoClick = function()
+        local result = processFieldsForForm( fields, formData )
+    end
 end
 
 local function openContactForm()
-	local formData = {}
-	formData.title = "Contact Form"
-	formData.formType = "contact"
+    local formData = {}
+    formData.title = "Contact Form"
+    formData.formType = "contact"
 
-	local questions = {}
+    local questions = {}
 
-	local contactMethod = {}
-	contactMethod.query = "What is your preferred contact method?"
-	contactMethod.name = "contact_method"
-	contactMethod.fieldType = "text"
-	table.insert( questions, contactMethod )
+    local contactMethod = {}
+    contactMethod.query = "What is your preferred contact method?"
+    contactMethod.name = "contact_method"
+    contactMethod.fieldType = "text"
+    table.insert( questions, contactMethod )
 
-	local message = {}
-	message.query = "What would you like to say?"
-	message.name = "message"
-	message.fieldType = "text"
-	table.insert( questions, message )
+    local message = {}
+    message.query = "What would you like to say?"
+    message.name = "message"
+    message.fieldType = "text"
+    table.insert( questions, message )
 
-	formData.questions = questions
+    formData.questions = questions
 
-	openForm( formData )
+    openForm( formData )
 end
 
 local function openFeedbackForm()
-	local formData = {}
-	formData.title = "Feedback Form"
-	formData.formType = "feedback"
+    local formData = {}
+    formData.title = "Feedback Form"
+    formData.formType = "feedback"
 
-	openForm( formData )
+    openForm( formData )
 end
 
 local function openBugReportForm()
-	local formData = {}
-	formData.title = "Bug Report Form"
-	formData.formType = "bug-report"
+    local formData = {}
+    formData.title = "Bug Report Form"
+    formData.formType = "bug-report"
 
-	openForm( formData )
+    openForm( formData )
 end
 
 local function openPlayerReportForm()
-	local formData = {}
-	formData.title = "Player Report Form"
-	formData.formType = "player-report"
+    local formData = {}
+    formData.title = "Player Report Form"
+    formData.formType = "player-report"
 
-	openForm( formData )
+    openForm( formData )
 end
 
-makeFormButton( "Contact", { ['x'] = 0, ['y'] = 50 }, openContactForm, Frame )
-makeFormButton( "Feedback", { ['x'] = 0, ['y'] = 100 }, openFeedbackForm, Frame )
-makeFormButton( "Bug Report", { ['x'] = 0, ['y'] = 150 }, openBugReportForm, Frame )
-makeFormButton( "Player Report", { ['x'] = 0, ['y'] = 200 }, openPlayerReportForm, Frame )
+local function openForms()
+    local Frame = vgui.Create( "DFrame" )
+    Frame:SetTitle( "Contact Forms" )
+    Frame:SetSize( 300, 300 )
+    Frame:Center()
+    Frame:MakePopup()
+
+    makeFormButton( "Contact", { ['x'] = 0, ['y'] = 50 }, openContactForm, Frame )
+    makeFormButton( "Feedback", { ['x'] = 0, ['y'] = 100 }, openFeedbackForm, Frame )
+    makeFormButton( "Bug Report", { ['x'] = 0, ['y'] = 150 }, openBugReportForm, Frame )
+    makeFormButton( "Player Report", { ['x'] = 0, ['y'] = 200 }, openPlayerReportForm, Frame )
+end
+
+concommand.Add( "cfc_forms", openForms )
