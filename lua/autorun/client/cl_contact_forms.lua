87dc140afffd8bed6a346ee9a8fe0b2c4d076583
Brandon Sturgeon <sturgeonb4@gmail.com>
2019-08-23 22:32:38 -0400

    Button changes

--- lua/autorun/client/cl_contact_forms.lua
+++ lua/autorun/client/cl_contact_forms.lua
@@ -113,7 +113,9 @@ local function makeSlidingScaleField( question, parent )
     for i=1, 5 do
         local Button = vgui.Create( "DImageButton", ButtonPanel )
         Button:Dock( LEFT )
-        Button:SetHeight( 100 )
+        Button:SetHeight( 50 )
+        Button:SetWidth( 50 )
+        Button:DockMargin( 0, 0, 5, 0 )
         Button:SetImage( "vgui/cfc_forms_fire_grayscale.png" )
         Button.DoClick = function()
             ButtonPanel.selectedValue = i
