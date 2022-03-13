2d8affd0542bbeb80dd18aa96b06b8d81af4f243
M.O.E.J. The Good <82342827+MOEJ-The-Good@users.noreply.github.com>
2022-03-12 19:20:25 -0500

    Comply to style guidelines.

--- lua/autorun/client/cl_contact_forms.lua
+++ lua/autorun/client/cl_contact_forms.lua
@@ -795,6 +795,8 @@ local GREEN = Color( 87, 242, 135 )
 local RED = Color( 237, 66, 69 )
 local YELLOW = Color( 254, 231, 92)
 local BLURPLE = Color( 88, 101, 242 )
+local DARK_GRAY = Color( 41, 41, 41 )
+local LIGHT_GRAY = Color( 150, 150, 150 )
 
 net.Receive( "CFC_ContactForms_SuccessAlert", function()
     surface.PlaySound( "buttons/button5.wav" )
@@ -810,42 +812,47 @@ net.Receive( "CFC_ContactForms_FailureAlert", function()
     )
 end )
 
-net.Receive("CFC_ContactForms_Alert", function()
+net.Receive( "CFC_ContactForms_Alert", function()
     local data = net.ReadTable()
-    local reporter = player.GetBySteamID(data["steam_id"])
-    local reported = player.GetBySteamID(data["reported_steam_id"])
+    local reporter = player.GetBySteamID( data.steam_id )
+    local reported = player.GetBySteamID( data.reported_steam_id )
+
+    if not IsValid( reporter ) or not IsValid( reported ) then return end
 
     local reporterName = reporter:GetName()
-    local reporterRankColor = team.GetColor(reporter:Team())
+    local reporterRankColor = team.GetColor( reporter:Team() )
 
     local reportedName = reported:GetName()
-    local reportedRankColor = team.GetColor(reported:Team())
+    local reportedRankColor = team.GetColor( reported:Team() )
 
     chat.AddText(
         reporterRankColor, reporterName,
         YELLOW, " has submitted a forms report on ",
         reportedRankColor, reportedName
     ) -- message via chat
+
     LocalPlayer():PrintMessage( 4, reporterName .. " has submitted a forms report on " .. reportedName ) -- message via HUD
-    local outerColor = Color(41, 41, 41)
-    local innerColor = Color(150, 150, 150)
+
     chat.AddText(
-        outerColor, "[", innerColor, "CFC Forms", outerColor, "] ",
-        Color(206,206,206), string.Left( data["message"], 99 ),
-        innerColor, "...\nFull message in console."
+        DARK_GRAY, "[", LIGHT_GRAY, "CFC Forms", DARK_GRAY, "] ",
+        Color( 206,206,206 ), string.Left( data.message, 99 ),
+        LIGHT_GRAY, "...\nFull message in console."
     ) -- prints reporter's message
     
-    local reportDataFields = {
-        Reporter = reporterName .. " (" .. data.steam_id .. ")",
-        Reported = reportedName .. " (" .. data.reported_steam_id .. ")",
-        Message = data["message"]
+    local decor = "+" .. string.rep( "-", 80 )
+    local reportDataFields = { -- Each field is a two-index table since string keys have an unpredictable order
+        { "Reporter", reporterName .. " (" .. data.steam_id .. ")" },
+        { "Reported", reportedName .. " (" .. data.reported_steam_id .. ")" },
+        { "Message", data.message }
     }
-    local decor = "+" .. string.rep("-", 80)
-    print(decor)
-    for fieldName, fieldValue in pairs( reportDataFields ) do
-        print( "| " .. fieldName .. "\t: " .. fieldValue )
+
+    MsgN( decor )
+
+    for _, field in ipairs( reportDataFields ) do
+        MsgN( "| " .. field[1] .. "\t: " .. field[2] )
     end
-    print(decor)
+
+    MsgN( decor )
 end)
 
 concommand.Add( "cfc_forms", CFCContactForms.openForms )
